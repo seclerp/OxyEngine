357fd239b0ddb8d4ba24df2dc4465598b088b106
_AnRu <craftmaniack@gmail.com>
2018-02-19 21:04:58 +0200

    Fixed entry.py example

--- Oxy.Framework.TestPlayer/scripts/entry.py
+++ Oxy.Framework.TestPlayer/scripts/entry.py
@@ -1,12 +1,18 @@
 # Using already defined modules
 from Oxy.Framework import Window, Input
 
-def update(dt):
+print "Hello from Python script"
+
+def checkExitByKey(dt):
   if Input.IsKeyPressed("escape"):
     Window.Exit()
 
-# Registerning OnUpdat elistener (defined above)
-Window.OnUpdate(update)
+def printDt(dt):
+  print "Delta time: ", dt
 
-# This line must be last, because of thrad blocking
+# Registerning OnUpdate listener (defined above)
+Window.OnUpdate(checkExitByKey)
+Window.OnUpdate(printDt)
+
+# This line must be last, because of thread blocking
 Window.Show(60.0)
