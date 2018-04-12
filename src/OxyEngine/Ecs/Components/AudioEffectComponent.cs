b54fbf087cdda3e9d1a2c20172cdbe4aef8943d1
Andrew Rublyov <craftmaniack@gmail.com>
2018-04-12 23:20:08 +0300

    Started AudioEffectComponent

---
+++ src/OxyEngine/Ecs/Components/AudioEffectComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Audio;
+
+namespace OxyEngine.Ecs.Components
+{
+  public class AudioEffectComponent : GameComponent
+  {
+    public SoundEffect AudioEffect { get; set; }
+
+    public void FireAndForget()
+    {
+      AudioEffect.Play();
+    }
+  }
+}
